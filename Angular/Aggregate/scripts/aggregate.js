9fbe506c462e94447f30c8c5e0f74dce202ca181
yuriyrozhok <yuriy.rozhok@gmail.com>
2018-05-28 11:40:45 +0200

    removed comments, added live preview of the number being entered

--- Angular/Aggregate/scripts/aggregate.js
+++ Angular/Aggregate/scripts/aggregate.js
@@ -2,24 +2,16 @@ angular.module('aggr', [])
     .controller('AggregateCtrl', AggregateCtrl);
     
 function getTotal(t, n) {
-    //return t.number + n.number;
     return t + n;
-    //return 12;
 }
 function AggregateCtrl($scope) {
     $scope.addNumber = function () {
-        //$scope.numbers.push(num);
-        //$scope.count = $scope.numbers.length;
-        //$scope.num += 1;
-        
-        //$scope.numbers.push({ number: $scope.num });
         $scope.numbers.push($scope.num);
     }
     $scope.getCount = function () {
         return $scope.numbers.length;
     }
     $scope.getSum = function () {
-        //return getTotal(0,0);
         return $scope.numbers.reduce(getTotal, 0);
     }
     $scope.init = function () {
